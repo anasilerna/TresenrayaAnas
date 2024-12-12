eb9a1ae606b6694fc6db3c5df315093ccbe10cbf
anasilerna <anasmejahad15@alumnes.ilerna.com>
2024-12-12 18:35:35 +0100

    Scanner y Random

--- src/tresenraya/tresenraya.java
+++ src/tresenraya/tresenraya.java
@@ -1,9 +1,14 @@
 package tresenraya;
 
+import java.util.Scanner;
+import java.util.Random;
+
 public class tresenraya {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
 
 	}
 
